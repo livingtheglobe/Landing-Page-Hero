dbd4a7d6380c3b666e402e8d446b38cc5a7b112e
livingtheglobe <irinandevon@gmail.com>
2025-11-28 21:01:00 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hero } from './components/Hero';
+
+const App: React.FC = () => {
+  return (
+    <main className="font-sans text-gray-900 bg-white">
+       <Hero />
+    </main>
+  );
+};
+
+export default App;
